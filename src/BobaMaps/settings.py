46ce8465614516b19042ba7911da6b7ecf6943b9
RiceAbove <evangelistagico@gmail.com>
2019-08-13 11:36:39 -0700

    working on yelp api

--- src/BobaMaps/settings.py
+++ src/BobaMaps/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # My apps
-    'shops'
+    'shops',
+    'pages'
 ]
 
 MIDDLEWARE = [
--- src/shops/views.py
+++ src/shops/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.conf import settings
+import requests
+import json
 
 # Create your views here.
 
-# shops_view()
+def request(host, path, api_key, url_params=None):
+    
+    return request.get()
+
+def search():
+    pass
+
+def get_business():
+    pass
+
+def query_api():
+    pass
+
 def shops_view(request):
     # YELP_API_KEY = settings.YELP_SECRET_KEY
-    # YELP_API_HOST = 'https://api.yelp.com/v3'
-    # YELP_SEARCH = 'businesses/search'
+    # YELP_API_HOST = 'https://api.yelp.com/v3/'
+    # SEARCH_PATH = 'businesses/search/'
+    # BUISNESS_PATH = 'business/' # Business ID will go after slash
     return render(request, 'shops.html', {})

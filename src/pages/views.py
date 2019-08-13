49c36524fd958102dfb39d3dbde1f2370f5958b2
RiceAbove <evangelistagico@gmail.com>
2019-08-13 04:05:09 -0700

    added comments

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# home_view()
 def home_view(request):
     return render(request, 'home.html')
 
+# about_view()
 def about_view(request):
     pass
--- src/shops/views.py
+++ src/shops/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
+
+# shops_view()
 def shops_view(request):
+    # YELP_API_KEY = settings.YELP_SECRET_KEY
+    # YELP_API_HOST = 'https://api.yelp.com/v3'
+    # YELP_SEARCH = 'businesses/search'
     return render(request, 'shops.html', {})

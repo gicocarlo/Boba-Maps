39522e1879d42e6eb1a01cf27809262b105246cb
RiceAbove <evangelistagico@gmail.com>
2019-08-13 22:49:59 -0700

    display boba shops

--- src/shops/views.py
+++ src/shops/views.py
@@ -15,7 +15,7 @@ BUSINESS_PATH = '/businesses/' #ID
 
 # DEFAULTS
 DEFAULT_LOCATION = 'San Francisco, CA'
-SEARCH_LIMIT = 3
+SEARCH_LIMIT = 5
 
 def request(host, path, api_key, params=None):
     params = params or {}
@@ -26,7 +26,7 @@ def request(host, path, api_key, params=None):
     response = requests.get(url=url, params=params, headers=headers)
     return response.json()
 
-def search(path, api_key):
+def search_boba(path, api_key):
     params = {
         'categories': 'bubbletea',
         'location': DEFAULT_LOCATION,
@@ -34,14 +34,34 @@ def search(path, api_key):
     }
     return request(YELP_API_HOST, path, api_key, params)
 
-def get_business(api_key, business_id):
+def get_boba_spot(api_key, business_id):
     business_path = BUSINESS_PATH + business_id
     return request(YELP_API_HOST, business_path, api_key)
 
+def boba_shops_id():
+    b = search_boba(SEARCH_PATH, YELP_API_KEY)
+    b_list = b['businesses']
+    b_list_id = []
+    for i in range(len(b_list)):
+        b_list_id.append(b_list[i]['id'])
+    return b_list_id
+
+def boba_shops():
+    boba_list = []
+    b_list_id = boba_shops_id()
+    for i in b_list_id:
+        response = get_boba_spot(YELP_API_KEY, i)
+        boba_shop = {
+            'name': response["name"],
+            'url': response["url"],
+            'addresses': response["location"]["address1"],
+            'rating': response["rating"]
+        }
+        boba_list.append(boba_shop)
+
+    return boba_list
+
 def shops_view(request):
-    b = search(SEARCH_PATH, YELP_API_KEY)
-    bus_id = b['businesses'][2]['id']
-    # print(bus_id)
-    response = get_business(YELP_API_KEY, bus_id)
-    # print(businesses)
-    return render(request, 'shops.html', response)
+    boba_list = boba_shops()
+    boba_dict = {'boba_list': boba_list}
+    return render(request, 'shops.html', boba_dict)

5b8963cae93fe0212f74adbe4d4b64d463cd5c70
RiceAbove <evangelistagico@gmail.com>
2019-08-13 18:26:37 -0700

    set up yelp api

--- src/shops/views.py
+++ src/shops/views.py
@@ -1,26 +1,45 @@
-from django.shortcuts import render
-from django.conf import settings
 import requests
 import json
+import urllib3
+from urllib.parse import quote
+from urllib.parse import urlencode
+from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 
+# YELP API KEY & HOST
+YELP_API_KEY = settings.YELP_SECRET_KEY
+YELP_API_HOST = 'https://api.yelp.com/v3/'
+
+# YELP API PATHS
+SEARCH_PATH = 'businesses/search/'
+BUISNESS_PATH = 'business/' # Business ID will go after slash
+
+# DEFAULTS
+DEFAULT_LOCATION = 'San Francisco, CA'
+SEARCH_LIMIT = 5
+
 def request(host, path, api_key, url_params=None):
-    
-    return request.get()
+    url_params = url_params or {}
+    url = '{}{}'.format(host, quote(path.encode('utf8')))
+    headers = {
+        'Authorization': 'Bearer {}'.format(str(api_key))
+    }
+    r = requests.get(url, headers=headers, params=url_params)
+    return r.json()
 
-def search():
-    pass
+def get_business(api_key, business_id):
+    buisness_path = BUSINESS_PATH + business_id
+    return request(API_HOST, buisness_path, YELP_API_KEY)
 
-def get_business():
-    pass
-
-def query_api():
-    pass
+def search(api_key, location):
+    url_params = {
+        'location': location.replace(' ', '+'),
+        'limit': SEARCH_LIMIT
+    }
+    return request(API_HOST, SEARCH_PATH, YELP_API_KEY, url_params=url_params)
 
 def shops_view(request):
-    # YELP_API_KEY = settings.YELP_SECRET_KEY
-    # YELP_API_HOST = 'https://api.yelp.com/v3/'
-    # SEARCH_PATH = 'businesses/search/'
-    # BUISNESS_PATH = 'business/' # Business ID will go after slash
+
     return render(request, 'shops.html', {})

ce4c56c0c0ccec2a1688c9fd90b6cbb1862dd9e7
RiceAbove <evangelistagico@gmail.com>
2019-08-23 17:24:28 -0700

    map changes to input location

--- src/shops/static/js/script.js
+++ src/shops/static/js/script.js
@@ -1,13 +1,26 @@
 var map;
 
 function initMap() {
-  var value = JSON.parse(document.getElementById('boba_loc').textContent);
-  console.log(value);
-  var list = JSON.parse(document.getElementById('boba_list').textContent);
-  console.log(list);
-
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.777, lng: -122.417},
-    zoom: 15
+    zoom: 12
   });
+
+  var value = JSON.parse(document.getElementById('boba_loc').textContent);
+  console.log(value);
+  var geocoding = new google.maps.Geocoder();
+  geocoding.geocode({'address': value}, (results, status) => {
+    if (status == 'OK') {
+      map.setCenter(results[0].geometry.location);
+      // var marker = new google.maps.Marker({
+      //   map: map,
+      //   position: results[0].geometry.location
+      // });
+    }
+    else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+  // var list = JSON.parse(document.getElementById('boba_list').textContent);
+  // console.log(list);
 }

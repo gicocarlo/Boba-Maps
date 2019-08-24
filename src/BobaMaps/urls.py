7129f4904f57aabe3f6d2ce7356ea9791db53cee
RiceAbove <evangelistagico@gmail.com>
2019-08-24 16:05:29 -0700

    add contact.html

--- src/BobaMaps/urls.py
+++ src/BobaMaps/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from shops.views import shops_view
-from pages.views import home_view, about_view
+from pages.views import home_view, about_view, contact_view
 
 urlpatterns = [
     path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path('shops/', shops_view, name='shops'),
-    path('about/', about_view, name='about')
+    path('about/', about_view, name='about'),
+    path('contact/', contact_view, name='contact')
 ]
--- src/pages/views.py
+++ src/pages/views.py
@@ -9,3 +9,6 @@ def home_view(request):
 # about_view()
 def about_view(request):
     return render(request, 'about.html')
+
+def contact_view(request):
+    return render(request, 'contact.html')

32daf5b760706bf3efc1ba99867d7dc3b2e09456
RiceAbove <evangelistagico@gmail.com>
2019-08-28 18:52:14 -0700

    add comments to pages

--- src/pages/forms.py
+++ src/pages/forms.py
@@ -1,5 +1,16 @@
 from django import forms
 
+'''
+ContactForm()
+
+Contains all of the fields attributes of the contact form in contact.html
+
+https://docs.djangoproject.com/en/dev/topics/forms/
+
+Args:
+    forms.Form: Basic field to access form fields and widgets
+'''
+
 class ContactForm(forms.Form):
     name    = forms.CharField(widget=forms.TextInput(attrs={'text':'', 'name':'name', 'class': 'form-control', 'placeholder': 'Name'}))
     email   = forms.EmailField(widget=forms.EmailInput(attrs={'name': 'email', 'class': 'form-control', 'placeholder': 'Email'}))
--- src/pages/views.py
+++ src/pages/views.py
@@ -3,41 +3,87 @@ from django.shortcuts import render
 from django.http import Http404
 from .forms import ContactForm
 
-# Create your views here.
+'''
+home_view()
 
-# home_view()
+Renders the home page [home.html]
+
+Args:
+    request: Determines if home_view is being requested
+
+Returns:
+    render: Loads the home page when GET request is recieved
+
+Raise:
+    Http404: Just for anything that goes wrong
+'''
+
 def home_view(request):
     try:
         return render(request, 'home.html')
     except:
         raise Http404('Page does not exist')
 
-# about_view()
+'''
+about_view()
+
+Renders the about page [about.html]
+
+Args:
+    request: Determines if about_view is being requested
+
+Returns:
+    render: Loads the about page when GET request is recieved
+
+Raise:
+    Http404: Just for anything that goes wrong
+'''
+
 def about_view(request):
     try:
         return render(request, 'about.html')
     except:
         raise Http404('Page does not exist')
 
-# contact_view()
+'''
+contact_view()
+
+Uses ContactForm() in forms.py to get valid data from user via a POST
+request. If valid, send message to me via email.
+
+https://docs.djangoproject.com/en/dev/topics/email/
+
+Args:
+    request: Determines if contact_view is being requested
+
+Returns:
+    render: Loads the contact page when a GET/POST request is recieved
+
+Raise:
+    Http404: Just for anything that goes wrong
+'''
+
 def contact_view(request):
-    form = ContactForm()
-    if request.method == "POST":
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            subject = form.cleaned_data['subject']
-            from_email = form.cleaned_data['email']
-            name = form.cleaned_data['name']
-            message = '{}\n\n{}'.format(form.cleaned_data['message'], name)
-            send_mail(
-                subject,
-                message,
-                from_email,
-                ['evangelistagico@gmail.com'],
-                fail_silently=False
-            )
-            form = ContactForm()
-    context = {
-        "contact_form": form
-    }
-    return render(request, 'contact.html', context)
+    try:
+        form = ContactForm()
+        if request.method == "POST":
+            form = ContactForm(request.POST)
+            if form.is_valid():
+                subject = form.cleaned_data['subject']
+                from_email = form.cleaned_data['email']
+                name = form.cleaned_data['name']
+                message = '{}\n\n{}'.format(form.cleaned_data['message'], name)
+                send_mail(
+                    subject,
+                    message,
+                    from_email,
+                    ['evangelistagico@gmail.com'],
+                    fail_silently=False
+                )
+                form = ContactForm()
+                context = {
+                    "contact_form": form
+                }
+        return render(request, 'contact.html', context)
+    except:
+        raise Http404('Page does not exist')

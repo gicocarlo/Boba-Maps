8a15f8e4e72fdedb228830e3941bb0021307be2d
RiceAbove <evangelistagico@gmail.com>
2019-08-29 14:20:18 -0700

    fixed static

--- src/BobaMaps/settings.py
+++ src/BobaMaps/settings.py
@@ -130,11 +130,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 

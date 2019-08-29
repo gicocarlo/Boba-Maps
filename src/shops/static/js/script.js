f1835f095bc0259c489afbff39c2aab84a5db22d
RiceAbove <evangelistagico@gmail.com>
2019-08-29 10:48:35 -0700

    comment js

--- src/shops/static/js/script.js
+++ src/shops/static/js/script.js
@@ -2,13 +2,31 @@ var map;
 var markers;
 var labels = '12345';
 
+/*
+initMap()
+
+Using Google Maps JavaScript API, this function creates the map onto the shops
+page.The coordinates are centered at Yelp HQ in San Francisco, CA
+
+https://developers.google.com/maps/documentation/javascript/tutorial
+*/
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.777, lng: -122.417},
+    center: {lat: 37.787, lng: -122.400},
     zoom: 11
   });
   changeMap();
 }
+
+/*
+changeMap()
+
+Uses Google's Geocoder API to convert the user input into coordinates which will
+be the new center of the map with a marker.
+
+https://developers.google.com/maps/documentation/javascript/geocoding
+*/
 
 function changeMap(){
   var address = JSON.parse(document.getElementById('boba_loc').textContent);
@@ -28,6 +46,14 @@ function changeMap(){
   markBobaShops();
 }
 
+/*
+markBobaShops()
+
+Using the coordinates queried from the Yelp API, add markers to each bubble tea
+shop
+
+https://developers.google.com/maps/documentation/javascript/markers
+*/
 function markBobaShops(){
   var bobaData = JSON.parse(document.getElementById('boba_list').textContent);
   for(let i = 0; i < bobaData.length; i++){
